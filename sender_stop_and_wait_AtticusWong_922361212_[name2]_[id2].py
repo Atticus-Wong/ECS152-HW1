105876852e2cd57a1e822f4a4a2be993dc94ba64
Atticus Wong <atticuswong@gmail.com>
2026-02-07 16:19:44 -0800

    add: throughput and per packet delay timers

--- sender_stop_and_wait_AtticusWong_922361212_[name2]_[id2].py
+++ sender_stop_and_wait_AtticusWong_922361212_[name2]_[id2].py
@@ -36,26 +36,36 @@ def solve():
     for i in range(0, len(contents), 1020):  
         stored_data.append(contents[i:i+1020])
     print(len(stored_data))
-    start = time.time()
+    start = time.time() # throughput timer
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("localhost", SENDER_PORT))
-    sock.settimeout(2.0)  # Add: timeout for recvfrom
+    sock.settimeout(2.0)
     idx = 0
+    packetDelays = []
     while idx < len(contents):
         seq_id = idx.to_bytes(4, byteorder='big')
         packet = seq_id + stored_data[idx // 1020]
-        sock.sendto(packet, ("localhost", RECEIVER_PORT))
+        packetStart = time.time()
+        sock.sendto(packet, ("localhost", RECEIVER_PORT)) # per packet delay timer starts here
         try:
             data, addr = sock.recvfrom(1028) 
+            # per packet delay timer stops here
+            packetStop = time.time()
             ack_id = int.from_bytes(data[:4], byteorder='big')
             print(f"Received ACK from {addr}")
             print(f"ACK ID (next expected byte): {ack_id}")
+            packetDelays.append(packetStop - packetStart)
+            idx = ack_id
         except socket.timeout:  
             time.sleep(2)
-        idx += 1020
         
-    end = time.time()
+    end = time.time() # throughput timer
     print(f"Total time to send all packets: {end - start}")
 
+    throughput, perPacketDelay = start - end, sum(packetDelays) / len(packetDelays)
+
+    print("Throughput: ", start - end)
+    print("Per Packet Delay: ", perPacketDelay)
+    print("Final Score:", 0.3*throughput/1000 + 0.7/perPacketDelay)
 if __name__ == "__main__":
     solve()
