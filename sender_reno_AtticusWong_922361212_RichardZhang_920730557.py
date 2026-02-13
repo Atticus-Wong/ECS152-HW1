773fc5be7ffd84683773ca41ebfdfafaa94ce774
Richard Zhang <rimzhang@ucdavis.edu>
2026-02-12 21:35:12 -0800

    revised per pkt delay calculation, removed extra variables, metric rounding

--- sender_reno_AtticusWong_922361212_RichardZhang_920730557.py
+++ sender_reno_AtticusWong_922361212_RichardZhang_920730557.py
@@ -1,15 +1,5 @@
 import time
 import socket
-from collections import defaultdict
-
-"""
-Useful resources: 
-    - https://book.systemsapproach.org/congestion/tcpcc.html
-    - https://canvas.ucdavis.edu/courses/1058429/files/folder/Lecture%20Slides?preview=29802473
-        - slide 18. maybe has incorrect info about what to do on timeout. slide says divide cwnd by 2 
-        but google and textbook say reset cwnd to 1
-    - https://www.geeksforgeeks.org/computer-networks/tcp-reno-with-example/
-"""
 
 HOST = "127.0.0.1"
 
@@ -19,6 +9,8 @@ SENDER_PORT = 5000
 PACKET_SIZE = 1024
 MESSAGE_SIZE = 1020
 SEQ_ID_SIZE = 4
+
+TIMEOUT = 3
 
 
 def open_file():
@@ -34,89 +26,69 @@ def solve():
     start = time.time() # throughput timer
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("localhost", SENDER_PORT))
-    sock.settimeout(1)
+    sock.settimeout(TIMEOUT)
 
     next_send_seq = 0
     last_ack_seq = 0
-    received_acks = {} #ack -> time received
-    sent_packets = {} #packet -> time received
+    ack_time = {} #ack seq -> time received
+    sent_time = {} #packet seq -> time sent, first transmission only
     cwnd = 1
-    ssthresh = 100
+    ssthresh = 64
     fast_recovery_flag = False
 
     dup_acks = 0
 
-    num_timeouts = 0
-    num_fast_retransmits = 0
-    num_cum_dup_acks = 0
-    fast_retransmit_freq_map = defaultdict(int) #seq id -> freq
 
     while last_ack_seq < len(contents):
         while (next_send_seq - last_ack_seq) // MESSAGE_SIZE < cwnd and next_send_seq < len(contents):
+            # Send as many packets as the window allows
             seq_id = next_send_seq.to_bytes(4, byteorder='big')
             packet = seq_id + stored_data[next_send_seq // MESSAGE_SIZE]
             sock.sendto(packet, ("localhost", RECEIVER_PORT))
-            print(f"Sent {next_send_seq//MESSAGE_SIZE}th packet")
-            if next_send_seq not in sent_packets:
-                sent_packets[next_send_seq] = time.time()
+            if next_send_seq not in sent_time:
+                sent_time[next_send_seq] = time.time()
             next_send_seq += MESSAGE_SIZE
         try:
             data, addr = sock.recvfrom(PACKET_SIZE)
             ack_id = int.from_bytes(data[:4], byteorder='big')
-            if ack_id not in received_acks:
-                received_acks[ack_id] = time.time()
 
             if ack_id == last_ack_seq:
-                num_cum_dup_acks += 1
                 dup_acks += 1
 
                 if dup_acks == 3:
-                    #Fast retransmit
-                    print(f"FAST RETRANSMIT {last_ack_seq // MESSAGE_SIZE}th packet")
-                    print("ENTERING FAST RECOVERY")
                     ssthresh = max(cwnd // 2, 1)
                     cwnd = ssthresh
-                    print(f"New cwnd: {cwnd}. New thresh: {ssthresh}")
                     # enter fast recovery. we increment cwnd by 1 for each dup ack
                     fast_recovery_flag = True
                     cwnd += 3
 
                     seq_id = last_ack_seq.to_bytes(4, byteorder='big')
                     packet = seq_id + stored_data[last_ack_seq // MESSAGE_SIZE]
-                    sock.sendto(packet, ("localhost", RECEIVER_PORT))
-                    num_fast_retransmits += 1
-                    fast_retransmit_freq_map[last_ack_seq  // MESSAGE_SIZE] += 1
+                    sock.sendto(packet, ("localhost", RECEIVER_PORT)) # fast retransmit
                 elif dup_acks > 3 and fast_recovery_flag:
-                    print(f"Fast recovery, cwnd: {cwnd}")
                     cwnd += 1
             elif ack_id > last_ack_seq:
+                for seq in range(last_ack_seq, ack_id, MESSAGE_SIZE):
+                    ack_time[seq] = time.time()
                 dup_acks = 0
                 last_ack_seq = ack_id
-                print(f"Received ACK up to {last_ack_seq//MESSAGE_SIZE}th packet")
                 if fast_recovery_flag:
                     cwnd = ssthresh # reset to ssthresh instead of 1
-                    print(f"Exiting fast recovery. Setting cwnd to {cwnd}")
                     fast_recovery_flag = False
                 else:
                     if cwnd < ssthresh: # slow start
-                        cwnd += 1 # cwnd grows exponentially
-                        print(f"Slow start, cwnd: {cwnd}")
+                        cwnd += 1 # cwnd grows exponentially, per ack received
                     else: # congestion avoidance
                         cwnd += 1 / cwnd # cwnd grows linearly, increment 1 MSS per RTT (we receive cwnd acks per RTT)
-                        print(f"Congestion avoidance, cwnd: {cwnd}")
-
-        except socket.timeout:
+        except socket.timeout: # havent't received packet in time, shrink window to size 1
             dup_acks = 0
-            num_timeouts += 1
             ssthresh = max(cwnd // 2, 1)
             cwnd = 1
             next_send_seq = last_ack_seq
-            print(f"TIMEOUT. thresh: {ssthresh}, cwnd: 1. Resending from {next_send_seq//MESSAGE_SIZE}")
-
+            fast_recovery_flag = False
 
 
     end = time.time()
-    print("\nentering termination protocol")
     empty_message = last_ack_seq.to_bytes(4, byteorder="big")
     sock.sendto(empty_message, ("localhost", RECEIVER_PORT))
 
@@ -133,30 +105,18 @@ def solve():
     sock.sendto(finack, ("localhost", RECEIVER_PORT))
     sock.close()
 
-    sorted_acks = sorted(list(received_acks.keys()))
-
     #calculate per packet delays
     packet_delays = []
-    for packet in sent_packets:
-        for ack in sorted_acks:
-            if ack > packet:
-                delay = received_acks[ack] - sent_packets[packet]
-                packet_delays.append(delay)
-                break
+    for seq in sent_time:
+        if seq in ack_time:
+            packet_delays.append(ack_time[seq] - sent_time[seq])
     
 
     throughput, per_pkt_delay = len(contents) / (end - start), sum(packet_delays) / len(packet_delays)
-    print("Throughput: ", throughput)
-    print("Per Packet Delay: ", per_pkt_delay)
-    print("Final Score:", 0.3*throughput/1000 + 0.7/per_pkt_delay)
-
-
-    #print("\nADDITIONAL STATS:")
-    #print("Number of timeouts:", num_timeouts)
-    #print("Number of cumulative duplicate acks:", num_cum_dup_acks)
-    #print("Number of fast retransmits:", num_fast_retransmits)
-    #if fast_retransmit_freq_map:
-    #    print("Frequency map of fast retransmits:", fast_retransmit_freq_map)
+    final_score = 0.3*throughput/1000 + 0.7/per_pkt_delay if per_pkt_delay > 0 else 0
+    print(f"Throughput: {throughput:.7f}")
+    print(f"Per Packet Delay: {per_pkt_delay:.7f}")
+    print(f"Final Score: {final_score:.7f}")
 
 
 if __name__ == "__main__":
